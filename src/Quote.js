f1eaa60e116ae6cbd0f7839ac840be195de11d2f
David <david.tomi2020@gmail.com>
2023-12-04 17:07:29 +0100

    add Quote.js
     some description

---
+++ src/Quote.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./quote.css";
+const Quote = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+        console.log(data);
+      });
+  }, []);
+  const showQuoteHandler = () => {
+    fetch("http://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+        console.log(data);
+      });
+  };
+  return (
+    <div className="quote">
+      <h1 className="title "> Random Quote Generator </h1>
+      <div className="house">
+        <h1 className="content"> {quote} </h1>
+        <div className="par"></div>
+        <h1 className="author"> {author} </h1>
+        <button onClick={showQuoteHandler} className="btn">
+          Generate Quote
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
